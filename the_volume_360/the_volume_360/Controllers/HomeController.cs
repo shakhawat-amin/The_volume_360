886f5b8fc93b46b790a248ca1a7510daf6ab45f5
shakhawat-amin <s.shakhawat.amin@gmail.com>
2016-12-10 03:04:56 +0600

    folder name finding
    How to access files and folder name. using DirectoryInfo, and Directory
    ect checked.

---
+++ the_volume_360/the_volume_360/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace the_volume_360.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public String FileTransfer()
+        {
+            String folder = "~/Views";
+            
+           
+            return  folder;
+        }
+        public ActionResult Gallery()
+        {
+            string path = Server.MapPath("~/Products");
+            List<string> Folders = new List<string>();
+
+          //  if (Directory.GetFiles(path, "*.jpg").Length > 0)
+            //    picFolders.Add(path);
+
+            if (Directory.GetFiles(path).Length > 0)
+                Folders.Add(path);
+            foreach (string dir in Directory.GetDirectories(path))
+            {
+                if (Directory.GetFiles(dir).Length > 0)
+                   // Folders.Add(dir);
+                Folders.Add(new DirectoryInfo(dir).Name);
+            }
+
+            return View(Folders);
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ the_volume_360/the_volume_360/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="the_volume_360.MvcApplication" Language="C#" %>
